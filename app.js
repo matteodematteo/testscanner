37ba23b3499c0c91eaf361437c7a0f461fb9c79b
matteodematteo <mattoteo96@gmail.com>
2026-07-15 17:57:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1069,7 +1069,7 @@
       method: "POST",
       body: JSON.stringify({
         id: payload.id,
-        goods_code: payload.goods_code,
+        goods_code: payload.barcode,
         italian_name: payload.italian_name,
         p_price: payload.p_price,
         s_price: payload.s_price,
